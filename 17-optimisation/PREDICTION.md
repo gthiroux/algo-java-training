cdd66246031fd32e6ca0e04184c64a49ce202e38
Garance THIROUX <thiroux.garance@gmail.com>
2025-12-06 17:41:27 +0100

    fin exo

--- 17-optimisation/PREDICTION.md
+++ 17-optimisation/PREDICTION.md
@@ -2,14 +2,20 @@
 
 ## Fonction `moyenneInef`
 
-**Problème identifié :** ___
+**Problème identifié :** 1ere boucle for inutile
 
-**Nombre d'opérations actuelles (pour n éléments) :** ___
+**Nombre d'opérations actuelles (pour n éléments) :** on prend i, on initialise somme à 0, on prend j et on boucle jusqu'à ce que j = i= 0,on augmente i, on initialise somme à 0 et on boucle jusqu'à ce que j=i =1 ,...,on augmente i, on initialise somme à 0 et on boucle jusqu'à ce que j=i =n
 
 **Version optimisée :**
+
 ```java
 public static double moyenneEff(int[] t) {
-    
+    double somme = 0;
+    for (int i = 0; i < t.length; i++) {
+        somme = somme + t[i];
+    }
+    return somme/t.length;
+
 }
 ```
 
@@ -17,12 +23,21 @@ public static double moyenneEff(int[] t) {
 
 ## Fonction `contientDoublonInef`
 
-**Problème identifié :** ___
+**Problème identifié :** condition de la 2eme boucle for et du if peut être simplifiée
 
 **Version optimisée :**
+
 ```java
 public static boolean contientDoublonEff(int[] t) {
-    
+       for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if ( t[i] == t[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
 }
 ```
 
@@ -30,12 +45,18 @@ public static boolean contientDoublonEff(int[] t) {
 
 ## Fonction `premierEtDernierInef`
 
-**Problème identifié :** ___
+**Problème identifié :** la boucle for pour savoir le premier du tableau est inutile puisqu'il aura toujours l'index 0
 
 **Version optimisée :**
+
 ```java
 public static String premierEtDernierEff(int[] t) {
-    
+    int premier = t[0];
+    int dernier = 0;
+    for (int i = 0; i < t.length; i++) {
+        dernier = t[i];
+    }
+    return premier + " et " + dernier;
 }
 ```
 
@@ -43,11 +64,20 @@ public static String premierEtDernierEff(int[] t) {
 
 ## Fonction `rechercheInef`
 
-**Problème identifié :** ___
+**Problème identifié :** la boucle va continuer même si on a trouvé une valeur
 
 **Version optimisée :**
+
 ```java
 public static int rechercheEff(int[] t, int val) {
-    
+    int index = -1;
+    for (int i = 0; i < t.length; i++) {
+        if (t[i] == val) {
+            index = i;
+            return index;
+        }
+    }
+    return index;
+
 }
 ```
--- 17-optimisation/src/Main.java
+++ 17-optimisation/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nombres = {3, 7, 2, 8, 5, 1, 9, 4};
+        int[] nombres = {3, 7, 2, 8, 5, 1, 9, 4 };
 
         System.out.println("moyenneInef : " + moyenneInef(nombres));
+        System.out.println("moyenneEff : " + moyenneEff(nombres));
         System.out.println("contientDoublonInef : " + contientDoublonInef(nombres));
+        System.out.println("contientDoublonEff : " + contientDoublonEff(nombres));
         System.out.println("premierEtDernierInef : " + premierEtDernierInef(nombres));
+        System.out.println("premierEtDernierEff: " + premierEtDernierEff(nombres));
         System.out.println("rechercheInef(5) : " + rechercheInef(nombres, 5));
+        System.out.println("rechercheEff(5) : " + rechercheEff(nombres, 5));
     }
 
     // PROBLEME 1 : Calcule la somme à chaque itération
@@ -21,11 +25,31 @@ public class Main {
         return moyenne;
     }
 
+    public static double moyenneEff(int[] t) {
+        double somme = 0;
+        for (int i = 0; i < t.length; i++) {
+            somme = somme + t[i];
+        }
+        return somme / t.length;
+
+    }
+
     // PROBLEME 2 : Compare chaque paire deux fois
     public static boolean contientDoublonInef(int[] t) {
         for (int i = 0; i < t.length; i++) {
             for (int j = 0; j < t.length; j++) {
                 if (i != j && t[i] == t[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean contientDoublonEff(int[] t) {
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (t[i] == t[j]) {
                     return true;
                 }
             }
@@ -47,6 +71,15 @@ public class Main {
         return premier + " et " + dernier;
     }
 
+    public static String premierEtDernierEff(int[] t) {
+    int premier = t[0];
+    int dernier = 0;
+    for (int i = 0; i < t.length; i++) {
+        dernier = t[i];
+    }
+    return premier + " et " + dernier;
+}
+
     // PROBLEME 4 : Continue après avoir trouvé
     public static int rechercheInef(int[] t, int val) {
         int index = -1;
@@ -57,4 +90,15 @@ public class Main {
         }
         return index;
     }
+    public static int rechercheEff(int[] t, int val) {
+    int index = -1;
+    for (int i = 0; i < t.length; i++) {
+        if (t[i] == val) {
+            index = i;
+            return index;
+        }
+    }
+    return index;
+
+}
 }
