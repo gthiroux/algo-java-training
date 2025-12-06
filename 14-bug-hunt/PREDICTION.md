bfa13849563384115a5ab8d8c3e1258bd3a9417c
Garance THIROUX <thiroux.garance@gmail.com>
2025-12-06 16:05:14 +0100

    fin exo

--- 14-bug-hunt/PREDICTION.md
+++ 14-bug-hunt/PREDICTION.md
@@ -2,46 +2,46 @@
 
 ## BUG 1 : fonction `moyenne`
 
-**Ligne suspecte :** ___
+**Ligne suspecte :** 28
 
-**Description du bug :** ___
+**Description du bug :** i=6
 
-**Ce qui va se passer :** ___
+**Ce qui va se passer :** i ne peut pas etre egale à 6 car le tableau ne possède pas d'index 6, plantage de java
 
-**Correction proposée :** ___
+**Correction proposée :** i<t.length
 
 ---
 
 ## BUG 2 : fonction `estTrie`
 
-**Ligne suspecte :** ___
+**Ligne suspecte :** 36-37
 
-**Description du bug :** ___
+**Description du bug :** dans la dernière itération, on compare t[4] à t[5] mais t.lenght = 5 donc l'index 5 n'existe pas (on commence à l'index 0)
 
-**Ce qui va se passer :** ___
+**Ce qui va se passer :** plantage de java car index > longueur du tableau
 
-**Correction proposée :** ___
+**Correction proposée :** i< t.length -1
 
 ---
 
 ## BUG 3 : fonction `inverse`
 
-**Ligne suspecte :** ___
+**Ligne suspecte :** 48
 
-**Description du bug :** ___
+**Description du bug :** en prenant l'index t.length-1-i, dans les dernières itérations, on reprend un index qu'on avait déjà modifié
 
-**Ce qui va se passer :** ___
+**Ce qui va se passer :** on va avoir un tableau non inversé
 
-**Correction proposée :** ___
+**Correction proposée :** double boucle for imbriquée
 
 ---
 
 ## BUG 4 : fonction `compter`
 
-**Ligne suspecte :** ___
+**Ligne suspecte :** 59
 
-**Description du bug :** ___
+**Description du bug :** avec le return, on sort de la fonction à la première valeur égale à val
 
-**Ce qui va se passer :** ___
+**Ce qui va se passer :** count sera égale à 1
 
-**Correction proposée :** ___
+**Correction proposée :** enlever le return count de la boucle for
--- 14-bug-hunt/src/Main.java
+++ 14-bug-hunt/src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nombres = {5, 2, 8, 1, 9};
+        int[] nombres = { 5, 2, 8, 1, 9, 2 };
 
         System.out.println("=== Test moyenne ===");
         System.out.println("moyenne : " + moyenne(nombres));
 
         System.out.println("\n=== Test estTrie ===");
-        int[] trie = {1, 2, 3, 4, 5};
-        int[] pasTrie = {1, 3, 2, 4, 5};
+        int[] trie = { 1, 2, 3, 4, 5 };
+        int[] pasTrie = { 1, 3, 2, 4, 5 };
         System.out.println("trie : " + estTrie(trie));
         System.out.println("pasTrie : " + estTrie(pasTrie));
 
         System.out.println("\n=== Test inverse ===");
-        int[] original = {1, 2, 3, 4, 5};
+        int[] original = { 1, 2, 3, 4, 5 };
         inverse(original);
         System.out.print("inverse : ");
-        for (int n : original) System.out.print(n + " ");
+        for (int n : original)
+            System.out.print(n + " ");
         System.out.println();
 
         System.out.println("\n=== Test compteur ===");
@@ -25,7 +26,10 @@ public class Main {
     // BUG 1 : Cette fonction devrait calculer la moyenne
     public static int moyenne(int[] t) {
         int somme = 0;
-        for (int i = 0; i <= t.length; i++) {
+        // for (int i = 0; i <= t.length; i++) {
+        // somme = somme + t[i];
+        // }
+        for (int i = 0; i < t.length; i++) {
             somme = somme + t[i];
         }
         return somme / t.length;
@@ -33,7 +37,13 @@ public class Main {
 
     // BUG 2 : Cette fonction devrait vérifier si le tableau est trié
     public static boolean estTrie(int[] t) {
-        for (int i = 0; i < t.length; i++) {
+        // for (int i = 0; i < t.length; i++) {
+        // if (t[i] > t[i + 1]) {
+        // return false;
+        // }
+        // }
+        // return true;
+        for (int i = 0; i < t.length - 1; i++) {
             if (t[i] > t[i + 1]) {
                 return false;
             }
@@ -43,20 +53,34 @@ public class Main {
 
     // BUG 3 : Cette fonction devrait inverser le tableau
     public static void inverse(int[] t) {
-        for (int i = 0; i < t.length; i++) {
-            int temp = t[i];
-            t[i] = t[t.length - 1 - i];
-            t[t.length - 1 - i] = temp;
+        // for (int i = 0; i < t.length; i++) {
+        // int temp = t[i];
+        // t[i] = t[t.length - 1 - i];
+        // t[t.length - 1 - i] = temp;
+        // }
+        for (int i = 0; i < t.length - 1; i++) {
+            for (int j = 0; j < t.length - 1 - i; j++) {
+                if (t[j] < t[j + 1]) {
+                    int tmp = t[j];
+                    t[j] = t[j + 1];
+                    t[j + 1] = tmp;
+                }
+            }
         }
     }
 
     // BUG 4 : Cette fonction devrait compter les occurrences
     public static int compter(int[] t, int val) {
         int count = 0;
+        // for (int i = 0; i < t.length; i++) {
+        // if (t[i] == val) {
+        // count++;
+        // return count;
+        // }
+        // }
         for (int i = 0; i < t.length; i++) {
             if (t[i] == val) {
                 count++;
-                return count;
             }
         }
         return count;
